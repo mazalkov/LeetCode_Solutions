2a29d4d4f3a0dafd226ecc38d864f89e016e1fdf
Andrew <32670729+mazalkov@users.noreply.github.com>
2021-01-25 10:32:46 +0000

    Create Two Sum.py

---
+++ Problems 1 to 50/Two Sum.py
@@ -0,0 +1,15 @@
+# https://leetcode.com/problems/two-sum/
+
+# Runtime: 36 ms, faster than 99.08% of Python3 online submissions for Two Sum.
+# Probably runs so fast because its iterating through a list, no fancy structs.
+
+# Memory Usage: 14.4 MB, less than 75.09% of Python3 online submissions for Two Sum.
+# Space usage not so efficient, hash maps may be better but this is a beginner problem.
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        
+        for i in range(len(nums)):
+            for j in range(i+1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i, j]
