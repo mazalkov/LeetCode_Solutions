168aee78549aea3ab1981fc13101f585ec68ecc1
Andrew <32670729+mazalkov@users.noreply.github.com>
2021-01-25 10:34:22 +0000

    Update Two Sum.py

--- Problems 1 to 50/Two Sum.py
+++ Problems 1 to 50/Two Sum.py
@@ -1,7 +1,7 @@
 # https://leetcode.com/problems/two-sum/
 
 # Runtime: 36 ms, faster than 99.08% of Python3 online submissions for Two Sum.
-# Probably runs so fast because its iterating through a list, no fancy structs.
+# Probably runs so fast because it's iterating through a list, no fancy structs.
 
 # Memory Usage: 14.4 MB, less than 75.09% of Python3 online submissions for Two Sum.
 # Space usage not so efficient, hash maps may be better but this is a beginner problem.
