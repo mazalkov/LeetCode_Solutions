c87cea8fa5408df669ce744301b5a97e3905585e
Andrew <32670729+mazalkov@users.noreply.github.com>
2023-10-19 08:24:28 +0100

    Create Backspace String Compare.py

---
+++ Problems 801 to 850/Backspace String Compare.py
@@ -0,0 +1,21 @@
+from typing import List
+
+
+class Solution:
+    def backspaceCompare(self, s: str, t: str) -> bool:
+
+        def reduce(x: str) -> List[str]:
+            x_stack = []
+            for char in x:
+                if char == "#":
+                    if x_stack:
+                        x_stack.pop()
+                else:
+                    x_stack.append(char)
+
+            return x_stack
+        
+        s_reduced = reduce(s)
+        t_reduced = reduce(t)
+
+        return s_mod == t_mod
