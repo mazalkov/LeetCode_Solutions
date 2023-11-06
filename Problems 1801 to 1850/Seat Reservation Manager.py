f05bf50525a13b349e671aa1b1a4bf0ecc4ad1ee
Andrew <32670729+mazalkov@users.noreply.github.com>
2023-11-06 20:16:58 +0000

    Create Seat Reservation Manager.py

---
+++ Problems 1801 to 1850/Seat Reservation Manager.py
@@ -0,0 +1,20 @@
+import heapq
+
+
+class SeatManager:
+
+    def __init__(self, n: int):
+        self.available = list(range(1, n+1))
+        heapq.heapify(self.available)
+
+    def reserve(self) -> int:
+        return heapq.heappop(self.available)
+
+    def unreserve(self, seatNumber: int) -> None:
+        heapq.heappush(self.available, seatNumber)
+
+
+# Your SeatManager object will be instantiated and called as such:
+# obj = SeatManager(n)
+# param_1 = obj.reserve()
+# obj.unreserve(seatNumber)
